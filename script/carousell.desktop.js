c32a3f65bc082dd0aacc0be166937049e04a1753
jayl-ch <jaychicogo328@gmail.com>
2026-01-01 14:57:51 +0800

    feat: add lightbox support

--- script/carousell.desktop.js
+++ script/carousell.desktop.js
@@ -1,5 +1,17 @@
-const carousellDesk = document.querySelector(".product__img-thumbnail");
+const carousellDesk = document.querySelector(".product__img-thumbnail.default");
 const thumbnail = document.querySelector(".product-img");
+
+const lightbox = document.querySelector(".product__img-wrapper-lightbox");
+const track = document.querySelector(".product-img-lightbox");
+const prev = document.querySelector(".prev-btn-lightbox");
+const next = document.querySelector(".next-btn-lightbox");
+
+const carousellLightbox = document.querySelector(
+  ".product__img-thumbnail.lightbox",
+);
+const closeLightbox = document.querySelector(".product__img-close-btn");
+const lightboxOverlay = document.querySelector(".lightbox-overlay");
+const thumbnailLightbox = document.querySelector(".product-img-lightbox");
 
 let currentIndex = 0;
 
@@ -25,4 +37,59 @@ carousellDesk.addEventListener("click", (e) => {
       break;
     }
   }
+});
+
+/* ============ lightbox ============ */
+let lightboxCurrentIndex = 0;
+
+carousellLightbox.addEventListener("click", (e) => {
+  const target = e.target;
+
+  for (const cls in positions) {
+    if (target.classList.contains(cls)) {
+      const index = positions[cls];
+
+      thumbnailLightbox.scrollBy({
+        left: index - lightboxCurrentIndex,
+      });
+
+      lightboxCurrentIndex = index;
+      break;
+    }
+  }
+});
+
+function hideLightbox(lbox, lboxOverlay) {
+  lbox.classList.remove("active");
+  lboxOverlay.classList.remove("active");
+}
+
+function showLightbox(lbox, lboxOverlay) {
+  lbox.classList.add("active");
+  lboxOverlay.classList.add("active");
+}
+
+const size = 10;
+
+prev.addEventListener("click", () => {
+  track.scrollBy({ left: -size, behavior: "smooth" });
+  if (lightboxCurrentIndex < 1) return;
+  lightboxCurrentIndex -= 464;
+});
+next.addEventListener("click", () => {
+  track.scrollBy({ left: size, behavior: "smooth" });
+  if (lightboxCurrentIndex > 1391) return;
+  lightboxCurrentIndex += 464;
+});
+
+closeLightbox.addEventListener("click", () => {
+  hideLightbox(lightbox, lightboxOverlay);
+});
+
+thumbnail.addEventListener("click", () => {
+  showLightbox(lightbox, lightboxOverlay);
+});
+
+lightboxOverlay.addEventListener("click", () => {
+  hideLightbox(lightbox, lightboxOverlay);
 });
